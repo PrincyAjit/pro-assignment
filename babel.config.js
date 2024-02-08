7152f8fd8230bc1bbb5da0554d0eac7211018323
Princy <princyajit898@gmail.com>
2024-02-08 16:57:00 +0530

    Updates

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-};
-
-// presets to be used for transforming the JS code to code that is compatible with browser.
-// preser-env is specifically for any ecma script features
-// preset-react is for react code.
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  verbose: true, // To give details about tests being run,additional debugging info.
-  transform: {
-    '^.+\\.js$': 'babel-jest', // use Babel for transforming js files.
-  },
-  // moduleDirectories: ["node_modules", "src"],
-  testEnvironment: 'jsdom',
-  // moduleFileExtensions: ["js", "json", "jsx", "node"],
-};
